9bb0535e9e19f580c1e88fc5749937b5211fd954
Shivam Gupta <shivamnaturelifecreativity369@gmail.com>
2026-08-10 14:47:34 +0530

    Time: 4 ms (98.48%), Space: 82.8 MB (80.92%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,23 +1,42 @@
-class Solution {
+ class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        // sort the costs in ascending order
-        Arrays.sort(costs);
 
-        int count = 0;
+        // Find the maximum cost
+        int maxCost = 0;
 
-        for(int cost : costs){
-            if(cost > coins){
-                return count;
-            }else{
-                count++;
-                coins -= cost; 
-            }
+        for (int cost : costs) {
+            maxCost = Math.max(maxCost, cost);
         }
 
-return count;
-         
+        // count[cost] = number of ice creams having this cost
+        int[] count = new int[maxCost + 1];
 
+        for (int cost : costs) {
+            count[cost]++;
+        }
 
-        
+        int total = 0;
+
+        // Go from cheapest to most expensive
+        for (int cost = 1; cost <= maxCost; cost++) {
+
+            if (count[cost] == 0) {
+                continue;
+            }
+
+            // Cannot afford even one ice cream of this cost
+            if (coins < cost) {
+                break;
+            }
+
+            // Number of ice creams we can buy
+            int buyKiya = Math.min(count[cost], coins / cost);
+
+            total += buyKiya;
+
+            coins -= buyKiya * cost;
+        }
+
+        return total;
     }
 }
