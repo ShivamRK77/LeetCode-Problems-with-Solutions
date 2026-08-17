34c5a0356f6999c9712fa8aa4780639d6090a6a8
Shivam Gupta <shivamnaturelifecreativity369@gmail.com>
2026-08-17 16:04:12 +0530

    Time: 5 ms (98.15%), Space: 47.1 MB (24.09%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -21,37 +21,45 @@ class Solution {
  // Better Approach is to use the Hashmap
 
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        // Count frequency
-        for (int i = 0; i < s.length(); i++) {
+        // // Count frequency
+        // for (int i = 0; i < s.length(); i++) {
 
-            char ch = s.charAt(i);
+        //     char ch = s.charAt(i);
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
 
-        // Find first unique character
-        for (int i = 0; i < s.length(); i++) {
+        // // Find first unique character
+        // for (int i = 0; i < s.length(); i++) {
 
-            if (map.get(s.charAt(i)) == 1) {
-                return i;
-            }
-        }
+        //     if (map.get(s.charAt(i)) == 1) {
+        //         return i;
+        //     }
+        // }
 
-        return -1;
+        // return -1;
 
 
  // Optimal Approach is to use Frequency Array
 
+
+ int[] frequency = new int[26];
+
+ for(int i = 0 ; i < s.length(); i++){
+
+    frequency[s.charAt(i) - 'a']++;
+    
+ }
+ for(int i = 0 ; i < s.length() ; i++){
+
+    if(frequency[s.charAt(i) - 'a'] == 1){
+        return i;
+    }
+ }
+ return -1;
  
-         
-
-
-
-
-
-
         
     }
 }
